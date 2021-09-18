b8d4e18dd07d813ead5793785b8cb25f1313d4fe
bryce <bryceperez31@gmail.com>
2021-09-18 16:59:23 -0700

    Add project files.

---
+++ BikeShop1.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeShop1.Dal;
+
+namespace BikeShop1.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerAdapter adapter = new CustomerAdapter();
+            List<Customer> customers = adapter.GetAll();
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.CustomerId + ":" + customer.FirstName + " " + customer.LastName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BikeShop1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BikeShop1.Startup))]
+namespace BikeShop1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
